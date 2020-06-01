239954e1df7753a60be39e8e43519475270eab00
B <xxflamegamingxx@gmail.com>
2020-06-01 18:20:31 -0400

    Added GPIO constants

--- constants.py
+++ constants.py
@@ -3,3 +3,24 @@
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5020
 expectedPacketLength = 3
+
+FR_DIR = 19
+FR_STEP = 26
+FL_DIR = 6
+FL_STEP = 13
+F_M0 = 10
+F_M1 = 9
+F_M2 = 11
+F_ENA = 22
+
+BR_DIR = 12
+BR_STEP = 16
+BL_DIR = 20
+BL_STEP = 21
+B_M0 = 25
+B_M1 = 8
+B_M2 = 7
+B_ENA = 24
+
+F_ENA = 22
+B_ENA = 24
