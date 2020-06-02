d25e41d849a2e16503bd205d0506f199d18f96bf
B <xxflamegamingxx@gmail.com>
2020-06-02 11:52:24 -0400

    Set global speeds to be used by threads later on

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 import socket
 import constants
 import dataTranslation
+import dataManipulation
+import stepper
+import threading
 
 packetLossCount = 0
-ySpeed = 0.0
-xSpeed = 0.0
-zRotation = 0.0
+
+frontRightWheelSpeed = 0.0
+frontLeftWheelSpeed = 0.0
+backRightWheelSpeed = 0.0
+backLeftWheelSpeed = 0.0
+
+stepper.initGPIO()
+
+#Start Motors disabled
+stepper.disableMotors()
 
 print ("Setting up UDP communication socket on " + constants.UDP_IP + ":" + str(constants.UDP_PORT))
 sock = socket.socket(socket.AF_INET, # Internet
@@ -21,7 +31,19 @@ while True:
     checkedPacket, valditity = dataTranslation.decodeData(data)
     if (valditity == True):
         print("Received valid packet! | {} , {} , {}".format(checkedPacket[0], checkedPacket[1], checkedPacket[2]))
+        #FL, FR, BL, BR
+        wheelspeeds = dataManipulation.calculateAllWheelSpeeds(checkedPacket[0], checkedPacket[1], checkedPacket[2])
+        frontLeftWheelSpeed = wheelspeeds[0]
+        frontRightWheelSpeed = wheelspeeds[1]
+        backLeftWheelSpeed = wheelspeeds[2]
+        backRightWheelSpeed = wheelspeeds[3]
     else:
         global packetLossCount
         packetLossCount += 1
         print ("ERROR: invalid packet received: {}".format(packetLossCount))
+        frontLeftWheelSpeed = 0.0
+        frontRightWheelSpeed = 0.0
+        backLeftWheelSpeed = 0.0
+        backRightWheelSpeed = 0.0
+
+    
