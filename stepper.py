d48e4a3ec2c06a6ad9d703a5eea954919a99b8c3
B <xxflamegamingxx@gmail.com>
2020-06-02 11:53:12 -0400

    Setup basic gpio control

---
+++ stepper.py
@@ -0,0 +1,46 @@
+import constants
+
+def initGPIO():
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(constants.B_M0, GPIO.IN)
+    GPIO.setup(constants.B_M1, GPIO.IN)
+    GPIO.setup(constants.B_M2, GPIO.IN)
+    GPIO.setup(constants.F_M0, GPIO.IN)
+    GPIO.setup(constants.F_M1, GPIO.IN)
+    GPIO.setup(constants.F_M2, GPIO.IN)
+
+    GPIO.setup(constants.F_ENA, GPIO.OUT)
+    GPIO.setup(constants.B_ENA, GPIO.OUT)
+
+    GPIO.setup(constants.FR_DIR, GPIO.OUT)
+    GPIO.setup(constants.FR_STEP, GPIO.OUT)
+
+    GPIO.setup(constants.FL_DIR, GPIO.OUT)
+    GPIO.setup(constants.FL_STEP, GPIO.OUT)
+
+    GPIO.setup(constants.BR_DIR, GPIO.OUT)
+    GPIO.setup(constants.BR_STEP, GPIO.OUT)
+
+    GPIO.setup(constants.BL_DIR, GPIO.OUT)
+    GPIO.setup(constants.BL_STEP, GPIO.OUT)
+
+def enableFrontMotors():
+    GPIO.output(constants.F_ENA, GPIO.LOW)
+
+def enableBackMotors():
+    GPIO.output(constants.B_ENA, GPIO.LOW)
+
+def disableFrontMotors():
+    GPIO.output(constants.F_ENA, GPIO.HIGH)
+
+def disableBackMotors():
+    GPIO.output(constants.B_ENA, GPIO.LOW)
+
+def enableMotors():
+    enableFrontMotors()
+    enableBackMotors()
+
+def disableMotors():
+    disableFrontMotors()
+    disableFrontMotors()
