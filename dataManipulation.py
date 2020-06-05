6a3f4b20fe16a745f177e50de499b94433f5cfd8
B <xxflamegamingxx@gmail.com>
2020-06-04 22:46:44 -0400

    Fixed direction method not computing correctly

--- dataManipulation.py
+++ dataManipulation.py
@@ -1,23 +1,27 @@
 import math
 
 def calculateWheelDirection(y,x):
-    return (math.atan2(y,x))
+    firstCalc = math.atan2(y,x)
+    if not (firstCalc == 0):
+        if (firstCalc/abs(firstCalc) == -1):
+            firstCalc = firstCalc + (2 * (math.pi)) 
+    return firstCalc
 
 def calculateWheelMagnitude(y,x):
     magnitude = math.sqrt(x ** 2.0 + y ** 2.0)
     return max(min(magnitude, 1.0), 0)
 
 def calculateFLWheelSpeed(direction, magnitude):
-    return (math.sin(direction + (1/4 * math.pi))) * magnitude
+    return ((math.sin(direction + (1/4 * math.pi)))) #* magnitude)
 
 def calculateFRWheelSpeed(direction, magnitude):
-    return (math.sin(direction - (1/4 * math.pi))) * magnitude
+    return (math.sin(direction - (1/4 * math.pi))) #* magnitude
 
 def calculateBLWheelSpeed(direction, magnitude):
-    return (math.sin(direction + (1/4 * math.pi))) * magnitude
+    return (math.sin(direction + (1/4 * math.pi))) #* magnitude
 
 def calculateBRWheelSpeed(direction, magnitude):
-    return (math.sin(direction - (1/4 * math.pi))) * magnitude
+    return (math.sin(direction - (1/4 * math.pi))) #* magnitude
 
 #Return FL, FR, BL, BR
 def calculateAllWheelSpeeds(y,x,z):
